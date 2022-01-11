1cdeb6da557392eb769e790a9ab7e4e827e31201
Scott Asher <scottasher@scottasher-mba.local>
2022-01-11 16:37:26 -0500

    fix up

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Users',      # Define that your audience are developers
-    'Topic :: Software Development :: Contacts',
+    'Intended Audience :: End Users/Desktop',      # Define that your audience are developers
+    'Topic :: Communications',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
